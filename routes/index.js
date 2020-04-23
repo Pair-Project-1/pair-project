2221a468093715ff24bb74cd3eb8530bef38a097
KangLeian <62584211+KangLeian@users.noreply.github.com>
2020-04-24 01:15:43 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const userRouter = require('./userRouter')
 const inputRouter = require('./inputRouter')
+const videoRouter = require('./VideoRouter.js');
 
 
 router.get('/', (req, res) => {
@@ -8,5 +9,6 @@ router.get('/', (req, res) => {
 })
 router.use('/input', inputRouter)
 router.use('/user', userRouter)
+routes.use('/videos', videoRouter);
 
 module.exports = router
